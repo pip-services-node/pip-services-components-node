9e0b18b7041ec3c8c9296dbc7e204e9e7afe3c51
Mark Makarychev <markmak77@gmail.com>
2018-08-04 13:08:33 +0300

    Commit of lock's TypeDoc.

--- src/lock/MemoryLock.ts
+++ src/lock/MemoryLock.ts
@@ -1,9 +1,23 @@
 /** @module lock */
 import { Lock } from './Lock';
 
+/**
+ * Stores a registry of locks in memory. A lock contains a string key (by which 
+ * the resource that is being locked can be identified) and the lock's 
+ * time to live (after which the lock is considered to be expired).
+ */
 export class MemoryLock extends Lock {
     private _locks: { [key: string]: number } = {};
 
+    /**
+     * Attempts to acquire a lock for the resource that is identified by the given key.
+     * 
+     * @param correlationId     not used.
+     * @param key               the key to identify the lock by.
+     * @param ttl               the lock's time-to-live.
+     * @param callback          the function that will be called with the result of the attempt or 
+     *                          with an error (if one is raised).
+     */
     public tryAcquireLock(correlationId: string, key: string, ttl: number,
         callback: (err: any, result: boolean) => void): void {
         let expireTime = this._locks[key];
@@ -17,6 +31,14 @@ export class MemoryLock extends Lock {
         }
     }
 
+    /**
+     * Releases the lock with the given key.
+     * 
+     * @param correlationId     not used.
+     * @param key               the key of the lock that is to be released.
+     * @param callback          (optional) the function to call once the lock has been released. Will be called 
+     *                          with <code>null</code>.
+     */
     public releaseLock(correlationId: string, key: string,
         callback?: (err: any) => void): void {
         delete this._locks[key];
--- src/lock/NullLock.ts
+++ src/lock/NullLock.ts
@@ -1,17 +1,46 @@
 /** @module lock */
 import { ILock } from './ILock';
-
+/**
+ * Null implementation of the [[ILock]] interface. Methods do not contain any logic and  
+ * simply accept the parameters passed to them. Can be used to cut dependecies while testing.
+ * 
+ * @see [[ILock]]
+ */
 export class NullLock implements ILock {
+    /**
+     * Null call to the [[ILock.tryAcquireLock tryAcquireLock]] method.
+     * 
+     * @param correlationId     not used.
+     * @param key               not used.
+     * @param ttl               not used.
+     * @param callback          will be called with <code>null, true<code>.
+     */
     public tryAcquireLock(correlationId: string, key: string, ttl: number,
         callback: (err: any, result: boolean) => void): void {
         callback(null, true);
     }
 
+    /**
+     * Null call to the [[ILock.acquireLock acquireLock]] method.
+     * 
+     * @param correlationId     not used.
+     * @param key               not used.
+     * @param ttl               not used.
+     * @param timeout           not used.
+     * @param callback          will be called with <code>null<code>.
+     */
     public acquireLock(correlationId: string, key: string, ttl: number, timeout: number,
         callback: (err: any) => void): void {
         callback(null);
     }
 
+    /**
+     * Null call to the [[ILock.releaseLock releaseLock]] method.
+     * 
+     * @param correlationId     not used.
+     * @param key               not used.
+     * @param callback          if given - will be called with <code>null<code>.
+     */
     public releaseLock(correlationId: string, key: string,
         callback?: (err: any) => void): void {
         if (callback) callback(null);
